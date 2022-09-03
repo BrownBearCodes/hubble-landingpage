f348e9bb7f1b862b32089d30f562eb894e329034
BrownBearCodes <112410226+BrownBearCodes@users.noreply.github.com>
2022-09-03 15:20:00 +0200

    Final Solution

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        violet: "#674BAF",
+        softmagenta: "#E882E8",
+      },
+    },
+  },
+  plugins: [],
+};
